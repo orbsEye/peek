345c08fb25458127322ec95c0092a64bbcda3de4
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2024-12-03 22:06:22 +0800

    update main.go
    reconstruction of methods

--- cmd/peek/main.go
+++ cmd/peek/main.go
@@ -1,29 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
 	"peek/api/handlers/coincap"
 	"peek/api/handlers/hackernews"
+
+	"github.com/gorilla/websocket"
 )
 
-func main() {
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	clientConn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, "Failed to upgrade to WebSocket", http.StatusInternalServerError)
+		return
+	}
+	defer clientConn.Close()
+
+	assets := []string{"bitcoin", "ethereum", "litecoin", "monero"}
+	coincap.StartWebSocketServer(assets, clientConn)
+}
+
+func main() {
 	http.HandleFunc("/hackernews", hackernews.HackerNewsHandler)
 
+	http.HandleFunc("/coincap", websocketHandler)
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-
 	port := ":8080"
-	println("Starting server on", port)
-	if err := http.ListenAndServe(port, nil); err != nil {
-		panic(err)
-	}
+	fmt.Printf("Starting server on %s\n", port)
 
-	coincap.CoincapInfo()
-	assets := []string{"bitcoin", "ethereum", "litecoin", "monero"}
+	go func() {
+		if err := http.ListenAndServe(port, nil); err != nil {
+			panic(err)
+		}
+	}()
+
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
-	go coincap.StartWebSocket(assets)
 	<-interrupt
+	fmt.Println("Server shutting down...")
 }
